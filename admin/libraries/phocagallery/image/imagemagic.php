62e089057fc74cd1b276c636627e6e3e7531a6fc
Jan <info@phoca.cz>
2021-02-08 21:46:52 +0100

    4.4.1
    4.4.1

--- admin/libraries/phocagallery/image/imagemagic.php
+++ admin/libraries/phocagallery/image/imagemagic.php
@@ -47,7 +47,7 @@ class PhocaGalleryImageMagic
 	* @param int $typeOut IMAGETYPE_type vystupniho obrazku
 	* @return bool Chyba kdyz vrati false
 	*/
-	public static function imageMagic($fileIn, $fileOut = null, $width = null, $height = null, $crop = null, $typeOut = null, $watermarkParams = array(), $frontUpload = 0, &$errorMsg) {
+	public static function imageMagic($fileIn, $fileOut = null, $width = null, $height = null, $crop = null, $typeOut = null, $watermarkParams = array(), $frontUpload = 0, &$errorMsg = '') {
 
 		$params 		= JComponentHelper::getParams('com_phocagallery') ;
 		$jfile_thumbs	=	$params->get( 'jfile_thumbs', 1 );
